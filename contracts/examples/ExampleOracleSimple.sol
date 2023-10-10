be1652ea386b2ef61608f0576cf5e51c55623a2b
MichaelWang <michael_wangguanyi@163.com>
2023-10-10 14:55:32 +0800

    upd

--- contracts/examples/ExampleOracleSimple.sol
+++ contracts/examples/ExampleOracleSimple.sol
@@ -12,16 +12,25 @@ import '../libraries/UniswapV2Library.sol';
 contract ExampleOracleSimple {
     using FixedPoint for *;
 
+    // 合约规定，两次update之间的时间间隔不能小于24小时
     uint public constant PERIOD = 24 hours;
 
+    // pair地址
     IUniswapV2Pair immutable pair;
+    // token0地址
     address public immutable token0;
+    // token1地址
     address public immutable token1;
 
+    // 记录上一次update后的token0的价格累计和
     uint    public price0CumulativeLast;
+    // 记录上一次update后的token1的价格累计和
     uint    public price1CumulativeLast;
+    // 记录上一次update的时间戳
     uint32  public blockTimestampLast;
+    // 本次update与上一次update之间的token0的TWAP
     FixedPoint.uq112x112 public price0Average;
+    // 本次update与上一次update之间的token1的TWAP
     FixedPoint.uq112x112 public price1Average;
 
     constructor(address factory, address tokenA, address tokenB) public {
@@ -37,19 +46,26 @@ contract ExampleOracleSimple {
         require(reserve0 != 0 && reserve1 != 0, 'ExampleOracleSimple: NO_RESERVES'); // ensure that there's liquidity in the pair
     }
 
+// 用于更新使用合约的内部记录时间及price0Cumulative的状态变量
     function update() external {
+        // 获取当前距离最近一次pair swap的时间间隔，及price0和price1的累计和
         (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) =
-            UniswapV2OracleLibrary.currentCumulativePrices(address(pair));
+                            UniswapV2OracleLibrary.currentCumulativePrices(address(pair));
+        // 计算当前时间戳与上一次执行update时的时间戳之间的时间价格（计算允许溢出）
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
 
         // ensure that at least one full period has passed since the last update
+        // 确保两次update之间的时间间隔大于等于间隔要求PERIOD
         require(timeElapsed >= PERIOD, 'ExampleOracleSimple: PERIOD_NOT_ELAPSED');
 
         // overflow is desired, casting never truncates
         // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
+        // token0在这段period的TWAP为t_now~t0的price0累计和 - t_{pre_update}~t0的price0累计和，再除以两次update之间的时间间隔（印证上述公式）
         price0Average = FixedPoint.uq112x112(uint224((price0Cumulative - price0CumulativeLast) / timeElapsed));
+        // token1在这段period的TWAP为t_now~t0的price1累计和 - t_{pre_update}~t0的price1累计和，再除以两次update之间的时间间隔（印证上述公式）
         price1Average = FixedPoint.uq112x112(uint224((price1Cumulative - price1CumulativeLast) / timeElapsed));
 
+        // 将本次price0Cumulative、price1CumulativeLast和blockTimestamp持久化到当前使用合约中
         price0CumulativeLast = price0Cumulative;
         price1CumulativeLast = price1Cumulative;
         blockTimestampLast = blockTimestamp;
